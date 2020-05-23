c921c390f1567f20792036cba59efc17278eda61
ElizabethGlz <elizabeth.10.xc@gmail.com>
2020-05-23 16:21:41 -0500

    Proyecto agregado a portafolio

---
+++ IEL/IEL/Vistas/JS/EditarUsuario.js
@@ -0,0 +1,60 @@
+﻿var User, Password, Nombre, ApellidoPaterno, ApellidoMaterno, Correo, Privilegio = "Alumno", Foto;
+var elegir = false;
+var actual;
+var contrasenia;
+$(document).ready(function () {
+    actual = localStorage.Usr;    
+    document.getElementById('EditUser').disabled = true;
+    IEL.Servicios.wsUsuario.getUsuarioByID(actual, cargar);
+    document.getElementById('EditPassword').focus();
+});
+
+function cargar(response) {
+    var usuario = JSON.parse(response);   
+    document.getElementById('EditUser').value = usuario.User;
+    document.getElementById('EditPassword').value = usuario.Password;
+    contrasenia = usuario.Password;
+    document.getElementById('EditNombre').value = usuario.Nombre;
+    document.getElementById('EditApellidoPaterno').value = usuario.ApellidoPaterno;
+    document.getElementById('EditApellidoMaterno').value = usuario.ApellidoMaterno;
+    document.getElementById('EditCorreo').value = usuario.Correo;
+}
+
+function cambiarAvatar(foto) {
+    var nueva = foto.src;
+    if (foto.id.localeCompare("imgHombre") == 0) {
+        document.getElementById('imgHombre').style.border = "thick solid blue";
+        document.getElementById('imgMujer').style.border = "none";
+    } else {
+        document.getElementById('imgHombre').style.border = "none";
+        document.getElementById('imgMujer').style.border = "thick solid blue";
+    }
+    var indice = nueva.indexOf('V');
+    Foto = nueva.substring((indice - 1), nueva.length);
+    elegir = true;
+}
+
+function editar() {
+    User = document.getElementById('EditUser').value;   
+    Password = document.getElementById('EditPassword').value;
+    var n = contrasenia.localeCompare(Password);
+    if (n == 0) {
+        Password = "igual";
+        
+    }
+    Nombre = document.getElementById('EditNombre').value;
+    ApellidoPaterno = document.getElementById('EditApellidoPaterno').value;
+    ApellidoMaterno = document.getElementById('EditApellidoMaterno').value;
+    Correo = document.getElementById('EditCorreo').value;
+    if (Password.localeCompare("") == 0 || Nombre.localeCompare("") == 0 || ApellidoPaterno.localeCompare("") == 0 || ApellidoMaterno.localeCompare("") == 0 || Correo.localeCompare("") == 0 || !elegir) {
+        alert("Todos los campos son obligatorios");
+    } else {
+        
+        IEL.Servicios.wsUsuario.update(User,Password,Nombre,ApellidoPaterno,ApellidoMaterno,Correo,Foto,Privilegio,volver);
+    }
+}
+
+function volver(response) {
+    alert("Usuario editado");
+    location.href = "CatalogoUsuarios.aspx";
+}
---
+++ IEL/IEL/Vistas/JS/Estadisticas.js
@@ -0,0 +1,86 @@
+﻿var usuario;
+//Llama low métodos de los web services necesarios para obtener los datos que serán cargados en la página.
+$(document).ready(function () {
+    usuario = $('#logear').text();
+    IEL.Servicios.wsUsuario.getUsuarioByID(usuario, cargarDatos);
+    IEL.Servicios.wsPractica.getTotalPresenteByUser(usuario, cargarEstadisticas);
+    IEL.Servicios.wsPractica.getTotalPasadoByUser(usuario, estadisticasPasado);
+    IEL.Servicios.wsPractica.getTotalVerbosByUser(usuario, estadisticasVerbos);
+    IEL.Servicios.wsPractica.getTotalVocabularioByUser(usuario, estadisticasVocabulario); 
+});
+
+
+var datosObtenidos;//Objeto de usuario correspondiente al usuario que desea consultar las estádistias.
+
+//Carga el nombre del usuario en la página. Recibe un JSON con el objeto usuario, y lo convierte a objeto.
+//Se acceden a los atributos del objeto para poder mostrar el nombre completo del usuario.
+function cargarDatos(resultado) {
+    
+    datosObtenidos = JSON.parse(resultado);
+   var nombre = datosObtenidos.Nombre+" "+datosObtenidos.ApellidoPaterno+" "+datosObtenidos.ApellidoMaterno;
+    document.getElementById("txtNombreAlumno").innerHTML = nombre;
+}
+//Carga el progreso total del presente simple.
+//Recibe la cantidad de preguntas contestadas del tema presente simple, este resultado es multiplicado por 10 para obtener el porcentaje.
+//Se asigna el porcentaje obtenido al elemento correspondiente para ser visualizado en la página.
+//El resultado obtenido, además es guardado en la variable global avanceps que será utilizada para calcular el progreso total.
+var avanceps;
+function cargarEstadisticas(resultado) {
+    var contestadas = JSON.parse(resultado);
+    var avancePS = contestadas * 10;
+    avanceps = avancePS;
+    document.getElementById("txtSimplePresent").innerHTML = "Simple present: " + avancePS + "%";
+}
+//Carga el progreso total del pasado simple.
+//Recibe la cantidad de preguntas contestadas del tema pasado simple, este resultado es multiplicado por 10 para obtener el porcentaje.
+//Se asigna el porcentaje obtenido al elemento correspondiente para ser visualizado en la página.
+//El resultado obtenido, además es guardado en la variable global avancePasado que será utilizada para calcular el progreso total.
+var avancePasado;
+function estadisticasPasado(resultado) {
+    var contestadas = JSON.parse(resultado);
+    var avancePS = contestadas * 10;
+    avancePasado = avancePS;
+    document.getElementById("txtSimplePast").innerHTML = "Simple past: " + avancePS + "%";
+}
+//Carga el progreso total del tema verbos.
+//Recibe la cantidad de preguntas contestadas del tema verbos, este resultado es multiplicado por 10 para obtener el porcentaje.
+//Se asigna el porcentaje obtenido al elemento correspondiente para ser visualizado en la página.
+//El resultado obtenido, además es guardado en la variable global avanceVerbos que será utilizada para calcular el progreso total.
+var avanceVerbos;
+function estadisticasVerbos(resultado) {
+    var contestadas = JSON.parse(resultado);
+    var avancePS = contestadas * 10;
+    avanceVerbos = avancePS;
+    document.getElementById("txtVerbs").innerHTML = "Verbs: " + avancePS + "%";
+}
+//Carga el progreso total del tema vocabulario.
+//Recibe la cantidad de preguntas contestadas del tema vocabulario, este resultado es multiplicado por 10 para obtener el porcentaje.
+//Se asigna el porcentaje obtenido al elemento correspondiente para ser visualizado en la página.
+//El resultado obtenido, además es guardado en la variable global avanceVc que será utilizada para calcular el progreso total.
+//Se manda llamar el método cargarAvance() para que puedan ser visualizadas las barras de progreso.
+var avanceVc;
+function estadisticasVocabulario(resultado) {
+    var contestadas = JSON.parse(resultado);
+    var avancePS = contestadas * 10;
+    avanceVc = avancePS;
+    document.getElementById("txtVocabulario").innerHTML = "Vocabulary : " + avancePS + "%";
+
+    cargarAvance();
+}
+//Se obtiene el progreso total calculando el promedio de todos los temas, para esto se hace uso de las variables avanceps,
+//avancePasado, avanceVerbos y avanceVc, dónde se almacenó el progreso total de caa uno de los temas.
+//Se le asigna el valor a las barras de progreso correspondientes a cada tema, y a la barra del progreso total. 
+function cargarAvance() {
+    var avance = (avanceps + avancePasado + avanceVerbos + avanceVc) / 4;
+    document.getElementById("barraPresente").value = avanceps;
+    document.getElementById("barraPasado").value = avancePasado;
+    document.getElementById("barraVerbo").value = avanceVerbos;
+    document.getElementById("barraVocabulario").value = avanceVc;
+    
+    avance = avance.toFixed(2);
+    document.getElementById("txtAvance").innerHTML = "Avance: " + avance + "%";
+    document.getElementById("barProgress").value = avance;
+
+
+
+}
